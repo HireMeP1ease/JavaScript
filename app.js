240d890d1c09a5de3c52b1f2f4ebfa6a73dc50f5
Van Tran <wadvan9876@gmail.com>
2021-07-16 12:43:36 -0600

    aaaaaaaaaa

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("test");
+function avg(arr){
+    let total = 0;
+    for (let num of arr){
+        total += num;
+    }
+    return total/arr.length;
+}
+
+function isPangra(sentence){
+    const testString = 'abcdefghijklmnopqrstuvwxyz';
+    for (let ch of testString){
+        if (sentence.indexOf(ch) === -1){
+            return false;
+        }
+        return true;
+    }
+}
